e393568b9cb0ef0fdca9914909ee6e02a26aea88
Emilio Haría <haria.informatica@gmail.com>
2024-10-15 12:47:48 +0100

    Counter

--- app/src/main/java/org/iesharia/mytabata/MainActivity.kt
+++ app/src/main/java/org/iesharia/mytabata/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MytabataTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,9 +30,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Counter(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "00",
         modifier = modifier
     )
 }
@@ -42,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MytabataTheme {
-        Greeting("Android")
+        Counter()
     }
 }
