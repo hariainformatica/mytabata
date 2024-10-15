69e417f132cc94c17b5f48da608b0d9183d30122
Emilio Haría <haria.informatica@gmail.com>
2024-10-15 12:50:06 +0100

    Counter

--- app/src/main/java/org/iesharia/mytabata/MainActivity.kt
+++ app/src/main/java/org/iesharia/mytabata/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,10 +33,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
-    Text(
-        text = "00",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "00",
+            modifier = modifier
+        )
+        Button(onClick = {}) {
+            Text(
+                text = "Pulsar"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
